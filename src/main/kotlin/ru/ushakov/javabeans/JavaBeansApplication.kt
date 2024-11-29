88891616a5593d8e638413e714be5a3c0cab8ef9
Kir Ushakov <imonare@gmail.com>
2024-11-29 23:27:39 +0300

    First commit

---
+++ src/main/kotlin/ru/ushakov/javabeans/JavaBeansApplication.kt
@@ -0,0 +1,11 @@
+package ru.ushakov.javabeans
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JavaBeansApplication
+
+fun main(args: Array<String>) {
+    runApplication<JavaBeansApplication>(*args)
+}
